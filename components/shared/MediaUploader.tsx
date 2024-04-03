60bde53d0c884be21a35a69d61d22c048b6e2103
akashcool2014.av@gmail.com <akash.verma@careerninja.in>
2024-04-02 20:59:35 -0400

    added upload preset

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="av_pixelBlend"
       options={{
         multiple: false,
         resourceType: "image",
